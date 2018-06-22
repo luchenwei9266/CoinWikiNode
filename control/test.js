f4cccfffb043cddd487e08fe5974a05b2731020a
lu <luchenwei9266@users.noreply.github.com>
2018-06-22 16:35:19 +0800

    增加脚本

--- control/test.js
+++ control/test.js
@@ -17,35 +17,55 @@ new AV.Query('Coins').descending('createdAt').first().then((res) => {
         tempList.push(res[key])
     }
     console.log(tempList.length);
-    tempList2 = tempList.slice(1800, 1923);
+    tempList2 = tempList.slice(1900, 2000);
     tempList2.forEach(item => {
         axios.get(CoinURL + item).then(result => {
             result = result.data;
-            // 如果本身没有中文翻译，本地翻译过去，再保存
-            // if (result.description.zh.length == 0) {
-            google.translate(result.description.en).then(text => {
-                let text_zh = '';
-                text.result.forEach(textItem => {
-                    text_zh = text_zh + textItem
-                })
-                let CoinText = AV.Object.extend('CoinText');
-                let coinText = new CoinText();
-                coinText.set('symbol', item).set("text_zh", text_zh).set("text_en", result.description.en);
-                coinText.save().then(data => {}, err => {
-                    console.log(err)
-                })
-            })
+            // 声明类型
+            var TodoFolder = AV.Object.extend('CoinsAllInfo');
+            // 新建对象
+            var todoFolder = new TodoFolder();
+            // 设置名称
+            todoFolder.set('data', result);
+            todoFolder.set('id', result.id);
+            todoFolder.save().then();
         }).catch(error => {
             console.log(error)
         });
-    })
+    });
 
 })
 
-function sleep(time = 0) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, time);
-    })
-};
+
+// let resultList = [];
+// new AV.Query('Coins').descending('createdAt').first().then((res) => {
+//     res = JSON.parse(res.attributes.list);
+//     let tempList = [];
+//     for (var key in res) {
+//         tempList.push(res[key])
+//     }
+//     console.log(tempList.length);
+//     tempList2 = tempList.slice(1800, 1923);
+//     tempList2.forEach(item => {
+//         axios.get(CoinURL + item).then(result => {
+//             result = result.data;
+//             // 如果本身没有中文翻译，本地翻译过去，再保存
+//             // if (result.description.zh.length == 0) {
+//             google.translate(result.description.en).then(text => {
+//                 let text_zh = '';
+//                 text.result.forEach(textItem => {
+//                     text_zh = text_zh + textItem
+//                 })
+//                 let CoinText = AV.Object.extend('CoinText');
+//                 let coinText = new CoinText();
+//                 coinText.set('symbol', item).set("text_zh", text_zh).set("text_en", result.description.en);
+//                 coinText.save().then(data => {}, err => {
+//                     console.log(err)
+//                 })
+//             })
+//         }).catch(error => {
+//             console.log(error)
+//         });
+//     })
+
+// })
